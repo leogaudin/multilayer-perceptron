9a78f6cf81b1094177aec8f97626d3c3a607588f
Léo <leogaudinoff@gmail.com>
2025-01-15 13:37:36 +0100

    refactor

--- losses.py
+++ losses.py
@@ -7,10 +7,8 @@ class CCE(Loss):
         super().__init__(self.cce, self.cce_prime)
 
     def cce(self, y_pred, y_true):
-        y_pred_clipped = np.clip(y_pred, 1e-15, 1 - 1e-15)
+        return -np.sum(y_true * np.log(y_pred))
 
-        return -np.sum(y_true * np.log(y_pred_clipped))
-
-    # ONLY WORKS IF SOFTMAX WAS APPLIED TO THE OUTPUT LAYER
+    # ONLY WORKS IF SOFTMAX WAS APPLIED TO Y_PRED
     def cce_prime(self, y_pred, y_true):
         return y_pred - y_true
