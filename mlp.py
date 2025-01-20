672bc0a0a26434260f53911f203f06c170a0a439
Léo <leogaudinoff@gmail.com>
2025-01-20 12:49:43 +0100

    test: add plotting of 3 models

--- mlp.py
+++ mlp.py
@@ -7,7 +7,6 @@ from model import Model
 from preprocessing import load_data, to_categorical
 from scaler import StandardScaler
 from stats import plot_multiple_losses
-import numpy as np
 
 
 def main():
@@ -53,7 +52,7 @@ def main():
 
     model_adam.save()
 
-    model_losses.append(test_losses)
+    model_losses.append(train_losses)
 
     model_sgd = Model(
         layers=[
@@ -85,11 +84,45 @@ def main():
 
     model_sgd.save()
 
-    model_losses.append(test_losses)
+    model_losses.append(train_losses)
+
+    model_rms = Model(
+        layers=[
+            layers.Dense((30, 800), initializers.he),
+            activations.ReLU(),
+            layers.Dense((800, 400), initializers.he),
+            activations.ReLU(),
+            layers.Dense((400, 2), initializers.he),
+            activations.Softmax()
+        ],
+        scaler=scaler,
+        loss=losses.CCE(),
+        optimizer=optimizers.RMSprop(),
+    )
+
+    (
+        train_losses,
+        test_losses,
+        train_accuracies,
+        test_accuracies
+    ) = model_rms.fit(
+        x_train=X_train,
+        y_train=y_train,
+        x_test=X_test,
+        y_test=y_test,
+        epochs=42,
+        batch_size=32,
+    )
+
+    model_rms.save()
+
+    model_losses.append(train_losses)
+
+    print(model_losses)
 
     plot_multiple_losses(
         losses=model_losses,
-        labels=["Adam", "SGD"],
+        labels=["Adam", "SGD", "RMSprop"],
         title="Losses",
         xlabel="Epochs",
         ylabel="Loss",
