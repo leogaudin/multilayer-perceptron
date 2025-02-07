3fbe3596220f03c319221b75de60dbab32cc70e7
Léo <leogaudinoff@gmail.com>
2025-02-07 16:53:03 +0100

    refactor

--- metadata.py
+++ metadata.py
@@ -1,10 +1,7 @@
 import torch
 
-
-device = None
-if torch.cuda.is_available():
-    device = torch.device('cuda')
-elif torch.mps.is_available():
-    device = torch.device('mps')
-else:
-    device = torch.device('cpu')
+device = torch.device(
+    "cuda" if torch.cuda.is_available() else
+    "mps" if torch.mps.is_available() else
+    "cpu"
+)
